49358f177c670aee6344c5d0b1a6ec2c0963005e
Max Ammann <maximilian.ammann@googlemail.com>
2018-05-21 11:40:26 +0200

    Make Nein Danke a little less intrusive

--- web-banner-intrusive/script.js
+++ web-banner-intrusive/script.js
@@ -1,6 +1,6 @@
 var close = function() {
   // console.log("test")
-  document.getElementsByClassName("gic2018-banner")[0].remove();
+  document.querySelector(".gic2018-banner").remove();
   localStorage.setItem("gic2018-banner", false);
 };
 
@@ -13,14 +13,14 @@ if (localStorage.getItem("gic2018-banner") === null) {
         <span class="gic2018-message-wrapper">Wir sind im Finale der Google.org Impact Challenge! 🚀 Es winken bis zu 500.000 € für unser Projekt und jede Stimme \
           zählt. Die Unterstützung ist nur einen Klick entfernt! 🎉🍻💥🎈🤸</span> \
         <div class="gic2018-btn-wrapper"> \
-          <span id="close" tabindex="1" class="gic2018-btn">Nein Danke</span> \
-          <a id="open" role="button" tabindex="0" class="gic2018-btn gic2018-btn-border" href="https://impactchallenge.withgoogle.com/deutschland2018/charities/digital-factory?v=NS0wLTItMTUyNjU3MTExNjY5NS40MDk3ODU4MjIwNjUyMzY4NjEtdm90aW5n" \
+          <span tabindex="1" class="gic2018-close gic2018-btn">Nein Danke</span> \
+          <a role="button" tabindex="0" class="gic2018-open gic2018-btn gic2018-btn-border" href="https://impactchallenge.withgoogle.com/deutschland2018/charities/digital-factory?v=NS0wLTItMTUyNjU3MTExNjY5NS40MDk3ODU4MjIwNjUyMzY4NjEtdm90aW5n" \
             target="_blank">Jetzt Abstimmen!</a> \
         </div> \
       </div> \
       ';
     document.body.appendChild(elem);
-    document.querySelector("#close").onclick = close;
-    document.querySelector("#open").onclick = close;
+    document.querySelector(".gic2018-close").onclick = close;
+    document.querySelector(".gic2018-open").onclick = close;
   });
 }
